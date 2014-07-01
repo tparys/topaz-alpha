ad5bda4d2dd73ebbdb4bba4c44bc1323d0b3ada7
tim <tim@b96395be-e1e1-45e4-9515-e269c6dcf6c4>
2014-07-01 01:34:05 +0000

    Topaz - Remove GPL code, change to 2-clause BSD license
    git-svn-id: svn://localhost/topaz@21 b96395be-e1e1-45e4-9515-e269c6dcf6c4

--- src/topaz/rawdrive.cpp
+++ src/topaz/rawdrive.cpp
@@ -1,6 +1,5 @@
 /**
  * File:   $URL $
- * Author: $Author $
  * Date:   $Date $
  * Rev:    $Revision $
  *
@@ -9,15 +8,28 @@
  * This file implements low level APIs used to communicate with Linux ATA
  * devices over SCSI translation layer using the SGIO ioctl.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Copyright (c) 2014, T Parys
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <unistd.h>
@@ -203,14 +215,20 @@ void rawdrive::check_tpm()
 {
   uint16_t id_data[256];
   
-  /* query identity */
+  // Query identify data
   get_identify(id_data);
   
-  /* ATA version must be >= 8 */
-  TOPAZ_DEBUG(1) printf("Searching for TPM Fingerprint\n");
-  if ((get_ata_ver(id_data) < 8) || ((id_data[48] & 0xC000) != 0x4000))
+  // Verify ATA version >= 8
+  TOPAZ_DEBUG(1) printf("Verifying ATA support\n");
+  if ((id_data[80] & ~((1 < 8) - 1)) == 0)
   {
-    /* no TPM */
+    throw topaz_exception("ATA device too old to report TPM presence");
+  }
+  
+  // Check for TPM presence
+  TOPAZ_DEBUG(1) printf("Searching for TPM Fingerprint\n");
+  if ((id_data[48] & 0xC000) != 0x4000)
+  {
     throw topaz_exception("No TPM Detected in Specified Drive");
   }
 }  
@@ -252,30 +270,6 @@ void rawdrive::get_identify(uint16_t *data)
     dump_id_string("Firmware", data + 23, 8);
     dump_id_string("Model", data + 27, 40);
   }
-}
-
-/**
- * get_ata_ver
- *
- * Decode ATA major version
- *
- * @param data Pointer to ATA identify data
- * @return ATA major version
- */
-int rawdrive::get_ata_ver(uint16_t *data)
-{
-  unsigned int mver;
-  
-  // The below will probably look familiar to Linux kernel devs ...
-  
-  if (data[80] == 0xFFFF)
-    return 0;
-  
-  for (mver = 14; mver >= 1; mver--)
-    if (data[80] & (1 << mver))
-      break;
-  
-  return mver;
 }
 
 /**
--- src/topaz/rawdrive.h
+++ src/topaz/rawdrive.h
@@ -3,7 +3,6 @@
 
 /**
  * File:   $URL $
- * Author: $Author $
  * Date:   $Date $
  * Rev:    $Revision $
  *
@@ -12,15 +11,28 @@
  * This file implements low level APIs used to communicate with Linux ATA
  * devices over SCSI translation layer using the SGIO ioctl.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Copyright (c) 2014, T Parys
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <stdint.h>
@@ -127,16 +139,6 @@ namespace topaz
      * @param data Data buffer (512 bytes)
      */
     void get_identify(uint16_t *data);
-    
-    /**
-     * get_ata_ver
-     *
-     * Decode ATA major version
-     *
-     * @param data Pointer to ATA identify data
-     * @return ATA major version
-     */
-    int get_ata_ver(uint16_t *data);
     
     /**
      * dump_id_string
